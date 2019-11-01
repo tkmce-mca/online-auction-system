17c174b3a26610c4525a920bc960f58671669dc1
rakeshnair5 <48026315+rakeshnair5@users.noreply.github.com>
2019-11-01 11:55:36 +0530

    database connection

---
+++ Connection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+namespace OnlineOction
+{
+    public class Connection
+    {
+        SqlConnection con;
+        SqlCommand cd;
+        public SqlDataReader dr;
+        SqlDataAdapter rd;
+        public void getcon()
+        {
+            try
+            {
+
+
+                con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=D:\rakesh\OnlineOction\OnlineOction\App_Data\onlineoction.mdf;Integrated Security=True;User Instance=True");
+                con.Open();
+            }
+            catch (Exception Ex)
+            {
+                String y = Ex.Message;
+                Console.Write(y);
+            }
+        }
+
+        public string save(String q)
+        {
+            try
+            {
+
+                getcon();
+                cd = new SqlCommand(q, con);
+                int y = cd.ExecuteNonQuery();
+                con.Close();
+                return y + "";
+            }
+            catch (Exception Ex)
+            {
+                return Ex.Message;
+            }
+        }
+        public void search(String q)
+        {
+            try
+            {
+
+                getcon();
+                cd = new SqlCommand(q, con);
+                dr = cd.ExecuteReader();
+                
+              
+            }
+            catch (Exception Ex)
+            {
+                String y = Ex.Message;
+                Console.Write(y);
+            }
+        }
+    }
+}
